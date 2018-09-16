9730be9c36e02e3d614df9e288180c39d4bce8bf
Dave Laing <dave.laing.80@gmail.com>
2018-09-16 20:00:46 +1000

    WIP

--- src/layout/Layout.hs
+++ src/layout/Layout.hs
@@ -40,6 +40,22 @@ runsDyck (x :< xs) = runDyck x <> runsDyck xs
 instance HasPrefix Run where
   prefix (Run p _ _ _) = p
 
+-- could use Foldable for this, but when in Rome...
+
+runDycks :: Run -> Cat Dyck
+runDycks (Run _ ds _ _) = ds
+
+runsDycks :: Cat Run -> Cat Dyck
+runsDycks Empty = Empty
+runsDycks (x :< xs) = runDycks x <> runsDycks xs
+
+runMismatch :: Run -> Cat LayoutMismatch
+runMismatch (Run _ _ _ es) = es
+
+runsMismatch :: Cat Run -> Cat LayoutMismatch
+runsMismatch Empty = Empty
+runsMismatch (x :< xs) = runMismatch x <> runsMismatch xs
+
 data Layout
   = E {-# unpack #-} !Delta
   | S {-# unpack #-} !Delta {-# unpack #-} !Run
@@ -62,6 +78,10 @@ dyckLayout d p t = S d $ Run p [t] t []
 boring :: Dyck -> Bool
 boring = views dyckLayoutMode (def ==)
 
+revCat :: Relative a => Cat a -> Cat a
+revCat Empty = Empty
+revCat (x :< xs) = snocCat (revCat xs) x
+
 instance Semigroup Layout where
   E 0 <> xs = xs
   xs <> E 0 = xs
@@ -76,10 +96,71 @@ instance Semigroup Layout where
       | otherwise -> V (d <> d') Empty lr $ Rev $ Cat.singleton (rel d rr)
     Right EQ -> S (d <> d') $ Run p (ds <> rel d ds') (ts <> rel d ts') (es <> rel d es')
     Right GT -> V (d <> d') (Cat.singleton lr) (rel d rr) Empty
+
   --S d lr@(Run p ds ts es) <> V d' l m r = case joinAndCompare p (prefix m) of
-  --   Left p'' | has _Empty r -> undefined -- S $ Run (d <> d') p'' 
+  --   Left p'' | has _Empty r -> undefined -- S $ Run (d <> d') p''
      -- ... -- TODO: resume here
-       
+
+  -- what happens if `ts` is boring in the S <> V or V <> S cases?
+  -- what do we do about the error cases?
+  --   - pack everything into an S?
+  --   - pack everything into whatever we would have had in the EQ case?
+  -- need to double check `rel`s in relation to the middle bit of Vs
+
+  -- TODO check the `Rev`s
+  -- TODO should be special casing when a we have S attaching to V where the V is Empty on that side?
+  --      or is it taken care of for us anyhow?
+
+  -- a
+  -- fg h ji/Rij
+  S d lr@(Run p ds ts es) <> V d' l m@(Run p' ds' ts' es') r = case joinAndCompare p p' of
+    Left p'' -> undefined
+    -- Empty a fghij/Rjihgf
+    Right LT ->
+      V (d <> d') Empty lr (Rev (revCat (rel d l)) <> Rev (Cat.singleton (rel d m)) <> rel d r)
+    -- Empty afgh ji/Rij
+    Right EQ ->
+      let
+        rdl = rel d l
+      in
+        V (d <> d') Empty (Run p (ds <> runsDycks rdl <> rel d ds') (ts <> runsDyck rdl <> rel d ts') (es <> runsMismatch rdl <> rel d es')) (rel d r)
+    -- afg h ji/Rij
+    Right GT ->
+      V (d <> d') (Cat.singleton lr <> rel d l) (rel d m) (rel d r)
+
+  V d l m r <> E d' = V (d <> d') l m r
+
+  -- ab c ed/Rde
+  -- f
+  V d l m@(Run p ds ts es) r@(Rev rr) <> S d' rr'@(Run p' ds' ts' es') = case joinAndCompare p p' of
+    Left p'' -> undefined
+    -- ab c fed/Rdef
+    Right LT -> V (d <> d') l m (Rev (Cat.singleton (rel d rr')) <> r)
+    -- ab (cdef) Empty
+    Right EQ ->
+      let
+        rrr = revCat rr
+      in
+        V (d <> d') l (Run p' (ds <> runsDycks rrr <> rel d ds') (ts <> runsDyck rrr <> rel d ts') (es <> runsMismatch rrr <> rel d es')) Empty
+    -- abcde f Empty
+    Right GT -> V (d <> d') (l <> Cat.singleton m <> revCat rr) (rel d rr') Empty
+
+  -- ab c ed/Rde
+  -- fg h ji/Rij
+  V d l m@(Run p ds ts es) r@(Rev rr) <> V d' l' m'@(Run p' ds' ts' es') r' = case joinAndCompare p p' of
+    Left p'' -> undefined
+    -- ab c jihgfed/Rdefghij
+    Right LT -> V (d <> d') l m (rel d r' <> Rev (Cat.singleton (rel d m')) <> Rev (revCat (rel d l')) <> r)
+    -- ab cdefgh ji/Rij
+    Right EQ ->
+      let
+        rrr = revCat rr
+        rdl' = rel d l'
+      in
+        V (d <> d') l (Run (prefix m) (ds <> runsDycks rrr <> runsDycks rdl' <> rel d ds') (ts <> runsDyck rrr <> runsDyck rdl' <> rel d ts') (es <> runsMismatch rrr <> runsMismatch rdl' <> rel d es')) (rel d r')
+    -- abcdefg h ji/Rij
+    Right GT -> V (d <> d') (l <> Cat.singleton m <> revCat rr <> rel d l') (rel d m') (rel d r')
+
 instance Monoid Layout where
   mempty = E 0
   mappend = (<>)
