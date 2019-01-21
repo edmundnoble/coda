73ce1d92891ed80025956a7c3294938c86ba0def
Edmund Noble <edmundnoble@gmail.com>
2019-01-20 20:09:53 -0500

    More parser stuff

--- src/parser/Syntax/Parser.hs
+++ src/parser/Syntax/Parser.hs
@@ -1,26 +1,36 @@
 {-# language DeriveAnyClass #-}
 {-# language GeneralizedNewtypeDeriving #-}
+{-# language FlexibleContexts #-}
 {-# language FlexibleInstances #-}
+{-# language LambdaCase #-}
 {-# language MultiParamTypeClasses #-}
+{-# language OverloadedStrings #-}
 {-# language GADTs #-}
 {-# language StandaloneDeriving #-}
 {-# language TypeFamilies #-}
+{-# language ViewPatterns #-}
 
 module Syntax.Parser where
 
+import Control.Applicative((<|>), some)
 import Control.Lens
+import Control.Monad(guard)
 import Data.Default
+import Data.Functor(($>))
+import Data.Text(Text)
 
 import Syntax.Dyck
+import Syntax.Name
 import Syntax.Token
 import Relative.Cat(Cat)
+import Relative.Delta(Delta(..), HasDelta(..))
 import Rev(Rev(..))
 
 import qualified Relative.Cat as Cat
 
 import qualified Text.Parsec.Pos
-import Text.Parsec.Prim hiding (parse)
-import Text.Parsec.Error(ParseError)
+import Text.Parsec.Combinator(optionMaybe)
+import Text.Parsec.Prim(ParsecT, Stream(..), runParser, tokenPrim, try)
 
 newtype TokenStream = TokenStream (Cat Token)
 
@@ -39,6 +49,9 @@ type TokParser a = ParsecT TokenStream () Identity a
 -- even if it weren't we have better info ourselves which we'll provide when
 -- we're using a better parser "library" we don't care about sourcenames for
 -- this reason.
+
+-- Turns out the above is kinda wrong, we can just store deltas in
+-- Text.Parsec.Pos.SourcePos and pull them out later ourselves.
 
 -- also, is it accurate to call what we're making an "abstract syntax tree" when
 -- we're only producing "levels" of the tree at a time? one odd consequence of
@@ -64,7 +77,7 @@ instance Semigroup P where
 parseDyck :: TokParser a -> Dyck -> Maybe a
 -- for now we eagerly fail if there are mismatched parens/braces/brackets.
 parseDyck p (Dyck o s (Rev c) e _ _) | Cat.null o && Cat.null c
-  = eitherToMaybe . runIdentity $ runParserT p () "" (TokenStream (s <> e))
+  = eitherToMaybe $ runParser p () "" (TokenStream (s <> e))
   where
     eitherToMaybe = either (const Nothing) Just
 parseDyck _ (Dyck _ _ _ _ _ _) = Nothing
@@ -92,7 +105,7 @@ data CAFDeclInfo = CAFDeclInfo
 data DataDeclInfo = DataDeclInfo
   deriving Show
 
-data Pat
+newtype Pat = Pat Name deriving Show
 
 data CaseBody
 
@@ -102,13 +115,51 @@ data InstanceBody
 
 data ModuleHeader
 
-tokenPos :: Text.Parsec.Pos.SourcePos -> Token -> Text.Parsec.Pos.SourcePos
-tokenPos t = Text.Parsec.Pos.newPos "" 0 t
+-- throw out old position info, shove delta into columns slot
+tokenPos ::
+  Text.Parsec.Pos.SourcePos -> Token -> TokenStream -> Text.Parsec.Pos.SourcePos
+tokenPos _ (delta -> Delta x) _
+  = Text.Parsec.Pos.newPos "" 0 x
 
-data TyName = TyName deriving Show
-data TermName = TermName deriving Show
+rawTok ::
+  Monad m =>
+  Text -> ParsecT TokenStream u m ()
+rawTok t = do
+  _ <- satisfy $ \case
+    Token _ t' -> t == t'
+    _ -> False
+  return ()
 
-data Expr = ExprDo Do | ExprLet Let | ExprWhere Where
+opt ::
+  Monad m =>
+  (Token -> Maybe a) -> ParsecT TokenStream u m a
+opt f = tokenPrim show tokenPos f
+
+anyTok ::
+  Monad m =>
+  ParsecT TokenStream u m Token
+anyTok = opt Just
+
+keyword ::
+  Monad m =>
+  Keyword -> ParsecT TokenStream u m Token
+keyword k = satisfy $ \case
+  TokenKeyword _ k' -> k == k'
+  _ -> False
+
+identP ::
+  Monad m =>
+  ParsecT TokenStream u m Name
+identP = opt $ \case
+  TokenName _ n -> Just n
+  _ -> Nothing
+
+satisfy ::
+  Monad m =>
+  (Token -> Bool) -> ParsecT TokenStream u m Token
+satisfy f = opt (\t -> guard (f t) $> t)
+
+data Expr = EDo Do | ELet Let | EWhere Where | ELam Name Expr | EIdent Name
   deriving Show
 
 data Do = Do [DoStatement] deriving Show
@@ -120,26 +171,42 @@ data Where = Where deriving Show
 data Purity = Pure | Impure
   deriving Show
 
-data DoStatement = DoStatement (Maybe TermName) Purity Expr
+data DoStatement = DoStatement (Maybe Pat) Purity Expr
   deriving Show
 
 data TopLevel
-  = Defn TermName CAFDeclInfo Expr
+  = TySig Name Expr
+  | Defn Name CAFDeclInfo Expr
   | Data DataDeclInfo
-  | TypeAlias TyName TyName
+  -- invariant: LHS of a type alias must be unqualified
+  | TyAlias Name Name
   deriving Show
 
-topLevelParser :: TokParser TopLevel
-topLevelParser = pure undefined
+topLevelP :: TokParser TopLevel
+topLevelP = undefined
 
-exprParser :: TokParser Expr
-exprParser = pure undefined
+doStmtP :: TokParser DoStatement
+doStmtP = pur <|> impur
+  where
+  pur = flip DoStatement Pure <$> (keyword KLet *> (Just <$> patP) <* rawTok "=") <*> exprP
+  impur = flip DoStatement Impure <$> (optionMaybe patP <* rawTok "<-") <*> exprP
 
-patParser ::  TokParser Pat
-patParser = pure undefined
+-- prime candidate for applicative parser composition
+exprP :: TokParser Expr
+exprP = lamP <|> letP <|> try appP <|> try (EIdent <$> identP)
+  where
+  appP = undefined -- EApp <$> exprP <*> exprP
+  lamP = ELam <$> (rawTok "\\" *> identP) <*> (rawTok "->" *> exprP)
+  letP = undefined
+
+patP ::  TokParser Pat
+patP = Pat <$> identP
+
+doP :: TokParser Do
+doP = keyword KDo *> (Do <$> some doStmtP)
 
 parse :: Dyck -> Parsed
-parse d = Parsed (parseDyck topLevelParser d)
+parse d = Parsed (parseDyck topLevelP d)
 
 retrieve :: Parsed -> Tag a -> Maybe a
 retrieve p TagTopLevel = parsedTopLevel p
